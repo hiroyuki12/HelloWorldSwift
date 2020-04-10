c4aa13cf35ffc9fbf349ca9456d52e13c5dcbcb4
hiroyuki12 <hiroyuki12@gmail.com>
2020-04-11 07:53:03 +0900

    Add MapKitViewController.swift.

---
+++ HelloWorldSwift/MapKitViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapKitViewController.swift
+//  HelloWorldSwift
+//
+//  Created by hiroyuki on 2020/04/11.
+//  Copyright © 2020 hiroyuki. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapKitViewController: UIViewController {
+    @IBOutlet weak var mkMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
