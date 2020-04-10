0bcaac2283d5f23e51f13675bae914b61a6831e0
hiroyuki12 <hiroyuki12@gmail.com>
2020-04-10 22:49:04 +0900

    Add LocationViewController.swift.

---
+++ HelloWorldSwift/LocationViewController.swift
@@ -0,0 +1,118 @@
+//
+//  LocationViewController.swift
+//  HelloWorldSwift
+//
+//  Created by hiroyuki on 2020/04/10.
+//  Copyright © 2020 hiroyuki. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import CoreLocation
+import LocalAuthentication
+import MapKit
+
+class LocationViewController: UIViewController {
+    @IBOutlet weak var labelLocation: UILabel!
+    @IBOutlet weak var labelLocation2: UILabel!
+    
+    // 緯度
+    var latitudeNow: String = ""
+    // 経度
+    var longitudeNow: String = ""
+    
+    // ロケーションマネージャ
+    var locationManager: CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // 位置情報を取得
+        // ロケーションマネージャのセットアップ
+        setupLocationManager()
+    }
+    
+    // Button 位置情報を取得
+    @IBAction func tapGetLocation(_ sender: Any) {
+        // マネージャの設定
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == .denied {
+            showAlert()
+        } else if status == .authorizedWhenInUse {
+            labelLocation.text = latitudeNow
+            labelLocation2.text = longitudeNow
+        }
+    }
+    
+    // ロケーションマネージャのセットアップ
+    func setupLocationManager() {
+        locationManager = CLLocationManager()
+        
+        // 権限をリクエスト
+        // 位置情報取得許可ダイアログの表示
+        guard let locationManager = locationManager else { return }
+        locationManager.requestWhenInUseAuthorization()
+        
+        // マネージャの設定
+        let status = CLLocationManager.authorizationStatus()
+        // ステータスごとの処理
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            // 位置情報取得を開始
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    // アラートを表示する
+    func showAlert() {
+        let alertTitle = "位置情報取得が許可されていません。"
+        let alertMessage = "設定アプリの「プライバシー > 位置情報サービス」から変更してください。"
+        let alert: UIAlertController = UIAlertController(
+            title: alertTitle,
+            message: alertMessage,
+            preferredStyle:  UIAlertController.Style.alert
+        )
+        // OKボタン
+        let defaultAction: UIAlertAction = UIAlertAction(
+            title: "OK",
+            style: UIAlertAction.Style.default,
+            handler: nil
+        )
+        // UIAlertController に Action を追加
+        alert.addAction(defaultAction)
+        // Alertを表示
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// 位置情報を取得
+extension LocationViewController: CLLocationManagerDelegate {
+
+    // 位置情報が更新された際、位置情報を格納する
+    // - Parameters:
+    //   - manager: ロケーションマネージャ
+    //   - locations: 位置情報
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        let latitude = location?.coordinate.latitude
+        let longitude = location?.coordinate.longitude
+        // 位置情報を格納する
+        self.latitudeNow = String(latitude!)
+        self.longitudeNow = String(longitude!)
+    }
+}
--- HelloWorldSwift/ViewController.swift
+++ HelloWorldSwift/ViewController.swift
@@ -16,28 +16,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelHello: UILabel!
     @IBOutlet weak var tableView: UITableView!
 //    @IBOutlet weak var wkWebView: WKWebView!
-    @IBOutlet weak var labelLocation: UILabel!
-    @IBOutlet weak var labelLocation2: UILabel!
     
     var articles: [[String: Any]] = [] //
-    
-    // 緯度
-    var latitudeNow: String = ""
-    // 経度
-    var longitudeNow: String = ""
-    // ロケーションマネージャ
-    var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        //
-        
-        
-        // 位置情報を取得
-        // ロケーションマネージャのセットアップ
-        setupLocationManager()
         
         // Face ID
         let context = LAContext()
@@ -90,31 +74,13 @@ class ViewController: UIViewController {
         task.resume() //実行する
         tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
     }
-
-
-    @IBAction func tapButton(_ sender: Any) {
-        labelHello.text = "Tap!"
-        print("Tap!")
-    }
-    
-    
     
     // Button backgroud Color
     @IBAction func tapButton2(_ sender: Any) {
         view.backgroundColor = UIColor.green
     }
     
-    // Button 位置情報を取得
-    @IBAction func tapGetLocationInfo(_ sender: Any) {
-        // マネージャの設定
-        let status = CLLocationManager.authorizationStatus()
-        if status == .denied {
-            showAlert()
-        } else if status == .authorizedWhenInUse {
-            labelLocation.text = latitudeNow
-            labelLocation2.text = longitudeNow
-        }
-    }
+
     
     // Button 画面遷移
     @IBAction func tapNextPage(_ sender: Any) {
@@ -126,47 +92,6 @@ class ViewController: UIViewController {
 //        nextVC.modalPresentationStyle = .fullScreen
 //        // presentする
 //        self.present(nextVC, animated: true, completion: nil)
-    }
-    
-    
-    // ロケーションマネージャのセットアップ
-    func setupLocationManager() {
-        locationManager = CLLocationManager()
-        
-        // 権限をリクエスト
-        // 位置情報取得許可ダイアログの表示
-        guard let locationManager = locationManager else { return }
-        locationManager.requestWhenInUseAuthorization()
-        
-        // マネージャの設定
-        let status = CLLocationManager.authorizationStatus()
-        // ステータスごとの処理
-        if status == .authorizedWhenInUse {
-            locationManager.delegate = self
-            // 位置情報取得を開始
-            locationManager.startUpdatingLocation()
-        }
-    }
-    
-    // アラートを表示する
-    func showAlert() {
-        let alertTitle = "位置情報取得が許可されていません。"
-        let alertMessage = "設定アプリの「プライバシー > 位置情報サービス」から変更してください。"
-        let alert: UIAlertController = UIAlertController(
-            title: alertTitle,
-            message: alertMessage,
-            preferredStyle:  UIAlertController.Style.alert
-        )
-        // OKボタン
-        let defaultAction: UIAlertAction = UIAlertAction(
-            title: "OK",
-            style: UIAlertAction.Style.default,
-            handler: nil
-        )
-        // UIAlertController に Action を追加
-        alert.addAction(defaultAction)
-        // Alertを表示
-        present(alert, animated: true, completion: nil)
     }
 }
 
@@ -201,22 +126,5 @@ extension ViewController: UITableViewDelegate {
 
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         return
-    }
-}
-
-// 位置情報を取得
-extension ViewController: CLLocationManagerDelegate {
-
-    // 位置情報が更新された際、位置情報を格納する
-    // - Parameters:
-    //   - manager: ロケーションマネージャ
-    //   - locations: 位置情報
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations.first
-        let latitude = location?.coordinate.latitude
-        let longitude = location?.coordinate.longitude
-        // 位置情報を格納する
-        self.latitudeNow = String(latitude!)
-        self.longitudeNow = String(longitude!)
     }
 }
