830d7600d6f2d7fb5f5c3d139a2c8beb450a19b6
hiroyuki12 <hiroyuki12@gmail.com>
2020-04-14 21:58:02 +0900

    Show Google Tap WebKit View.

--- HelloWorldSwift/ViewController.swift
+++ HelloWorldSwift/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
     
+  }
+  
+// ①セグエ実行前処理
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//    print ("aaa")
+//    print (segue.identifier)
+    // ②Segueの識別子確認
+    //if segue.identifier == "MyWebView" {
+      // ③遷移先ViewCntrollerの取得
+      let webView = segue.destination as! WebViewController
+      // ④遷移先に渡す値の設定
+      webView.url = "http://www.google.com/"
+    //}
+  }
+  
     // Button 画面遷移
 //    @IBAction func tapNextPage(_ sender: Any) {
 //        // Storyboardのインスタンスを名前指定で取得する
--- HelloWorldSwift/WebViewController.swift
+++ HelloWorldSwift/WebViewController.swift
@@ -20,8 +20,11 @@ class WebViewController: UIViewController {
 
     // Do any additional setup after loading the view.
     
-    //if let url = URL(string: self.url) {
-    if let url = URL(string: "https://www.apple.com/jp/swift/") {  // URL文字列の表記間違いなどで、URL()がnilになる場合があるため、nilにならない場合のみ以下のload()が実行されるようにしている
+    print("Hello!")
+    print(url)
+    
+    if let url = URL(string: self.url!) {
+    //if let url = URL(string: "https://www.apple.com/jp/swift/") {  // URL文字列の表記間違いなどで、URL()がnilになる場合があるため、nilにならない場合のみ以下のload()が実行されるようにしている
       let request = URLRequest(url: url)
       //self.wkWebView.load(URLRequest(url: url))
       wkWebView.load(request)
