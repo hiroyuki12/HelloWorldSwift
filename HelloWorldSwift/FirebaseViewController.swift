514502e9546db6939848da2f67f08983368c289f
hiroyuki12 <hiroyuki12@gmail.com>
2022-02-11 13:09:47 +0900

    Check Firestore Database data add OK.

--- HelloWorldSwift/FirebaseViewController.swift
+++ HelloWorldSwift/FirebaseViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 //import FirebaseDatabase
 //import FirebaseAuth
 
-//import FirebaseFirestore
+import FirebaseFirestore
 
 class FirebaseViewController: UIViewController {
   @IBOutlet weak var txtId: UITextField!
@@ -61,6 +61,13 @@ class FirebaseViewController: UIViewController {
 //      }
 //    }
     
+      
+          let db = Firestore.firestore()
+          db.collection("users").addDocument(data: ["name": "hoge2"]) { error in
+            if let error = error { print("エラーが起きました") }
+            else { print("ドキュメントが保存できました") }
+          }
+      
     print("finished !!!")
   }
   
--- Podfile
+++ Podfile
@@ -3,9 +3,10 @@ target 'HelloWorldSwift' do
 
   pod 'SwiftyDropbox'
   pod 'BoxSDK', '~> 4.0'
-#  pod 'Firebase/Core'
+  pod 'Firebase/Core'
 #  pod 'Firebase/Database'
 #  pod 'Firebase/Firestore'
+  pod 'FirebaseFirestore', :git => 'https://github.com/invertase/firestore-ios-sdk-frameworks.git', :tag => '8.12.1'
 #  pod 'Firebase/Auth'
   pod 'Swifter', :git => 'https://github.com/mattdonnelly/Swifter.git'
   pod 'OAuthSwift'
